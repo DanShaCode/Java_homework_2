3a0bc92d408af648582787027ad2faefe530382c
Danis Sharipov <danshadance@mail.ru>
2023-03-07 09:19:26 +0600

    initial commit

---
+++ task1/Task1.java
@@ -0,0 +1,37 @@
+package task1;
+
+public class Task1 {
+
+    private static String selectFrom(String ignore, String[] str) {
+
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < str.length; i++) {
+            if (i == 0)
+                sb.append("" + str[i]);
+            else if (!str[i].contains(ignore))
+                sb.append(" AND" + str[i]);
+        }
+
+        String filtered = new String(sb)
+        .replaceAll(":", "=")
+        .replaceAll("'", "");
+
+
+        return filtered;
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println("\033[H\033[2J");
+
+        String original = "'name:Ivanov', 'country:Russia', 'city:Moscow', 'age:null'";
+        String[] str = original.split(",");
+
+        String filtered = selectFrom("null", str);
+
+        System.out.println("The original line: 'name:Ivanov', 'country:Russia', 'city:Moscow', 'age:null'");
+        System.out.println();
+        System.out.println("SELECT * FROM line: " + filtered);
+    }
+}
---
+++ task2/Task2.java
@@ -0,0 +1,50 @@
+package task2;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Random;
+import java.util.logging.FileHandler;
+import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
+
+public class Task2 {
+    public static int[] FillArr(int[] arr, Random rnd){
+        for (int i = 0; i < arr.length; i++){
+            arr[i] = rnd.nextInt(1, 10);
+        }
+        return arr;
+    }
+    public static void main(String[] args) throws SecurityException, IOException {
+
+        System.out.println("\033[H\033[2J");
+
+        Logger logger = Logger.getLogger(Task2.class.getName());
+        SimpleFormatter sf = new SimpleFormatter();
+        FileHandler fh = new FileHandler("task2/BubbleSortLogs.txt");
+        fh.setFormatter(sf);
+        logger.addHandler(fh);
+
+        Random rnd = new Random();
+
+        int[] numbers = new int [rnd.nextInt(10,20)];
+        FillArr(numbers, rnd);
+
+        int temp = 0;
+        int count = 0;
+
+        for (int i = 0; i < numbers.length; i++) {
+            for (int j = 0; j < numbers.length - 1; j++) {
+                if (numbers[j] > numbers[j + 1]) {
+                    temp = numbers[j];
+                    numbers[j] = numbers[j + 1];
+                    numbers[j + 1] = temp;
+                    count++;
+                    String stringCounter = Integer.toString(count);
+                    String arr = Arrays.toString(numbers);
+                    logger.info("(" + stringCounter + ")" + " Iteration " + arr);
+                }
+            }
+        }
+
+    }
+}
